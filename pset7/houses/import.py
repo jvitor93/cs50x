1d657bfb43a82ce3f7dbb56ff4564f6727768be2
joaolopes <lopesjvm@gmail.com>
2020-05-06 02:57:31 -0300

    Finished pset7 - SQL

--- pset7/houses/import.py
+++ pset7/houses/import.py
@@ -1,1 +1,42 @@
-# TODO
+# This progrm should import houses data from a CSV file
+#   * Print error if the incorrect number of command-line arguments is inputed
+#   * Assume the CSV file to contain the collumns 'name', 'house' and 'birth'
+# For each student in the file, update a SQL database in a file called students.db
+#   *Separate name in  the database into 'first', 'middle' and 'last'
+#   *For students without middle name, leave this field as NULL in the database
+
+# Loadind libraries
+from cs50 import SQL
+import csv
+from sys import argv, exit
+
+
+# Checking for input error
+if not len(argv) == 2:
+    print("Incorrect number of in line arguments")
+    exit()
+# Initializing database file
+db = SQL("sqlite:///students.db")
+# Getting data
+with open(argv[1], "r") as datafile:
+    table = csv.DictReader(datafile)
+
+    #iterating through students
+    for student in table:
+        #getting birth and house data
+        birth = int(student["birth"])
+        house = student['house']
+        #getting fullname list
+        fullname = student['name'].split()
+        #separating fullname into first, middle, last
+        if len(fullname) == 3:
+            first = fullname[0]
+            middle = fullname[1]
+            last = fullname[2]
+        else:
+            first = fullname[0]
+            middle = None
+            last = fullname[1]
+        #inserting line into the SQL database
+        db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES (?,?,?,?,?)",
+                   first, middle, last, house, birth)
--- pset7/houses/roster.py
+++ pset7/houses/roster.py
@@ -1,1 +1,36 @@
-# TODO
+# The program prints a list of students in a given house
+# in alphabetical order
+#   *Accept the house name as command-line, otherwise exit
+#   * Query the students table in the SQL database
+# Print out students fullname, birth and year formatted as:
+#   Harry James Potter, born 1980
+# Each student in one line
+# Ordered by last name
+
+
+# Loadind libraries
+import csv
+from sys import argv
+from cs50 import SQL
+
+# Getting database
+db = SQL("sqlite:///students.db")
+
+# Checking for input error
+if not len(argv) == 2:
+    print("Incorrect number of in line arguments")
+    exit()
+
+house = argv[1]
+
+# Execute query to get list os students
+
+students = db.execute(
+    "SELECT first, middle, last, birth FROM students WHERE house = ? ORDER by last ASC, first ASC;", house)
+
+for student in students:
+    if student['middle'] != None:
+        print(student['first'], student['middle'],
+              student['last'] + f", born {student['birth']}")
+    else:
+        print(student['first'], student['last'] + f", born {student['birth']}")
